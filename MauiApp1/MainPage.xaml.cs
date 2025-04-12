d209157ae32040e3ed5205441c205cf584bfadab
sergioeval <sergioev@gmail.com>
2025-04-12 12:18:43 -0600

    initial commit

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿namespace MauiApp1;
+using System.Text.RegularExpressions;
+
+public partial class MainPage : ContentPage
+{
+	// int count = 0;
+	private bool IsPasswordValid(string pass1, string pass2) {
+		// check if any is empty or null
+		if (string.IsNullOrEmpty(pass1) || string.IsNullOrEmpty(pass2)) {
+			return false;
+		}
+		// check if is the same pass 1 and pass2
+		if (pass1 != pass2) {
+			return false;
+		}
+		var pattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z\d]).+$";
+		return Regex.IsMatch(pass1, pattern);
+	}
+
+	public MainPage() {
+		InitializeComponent();
+	}
+
+	private void OnSubmitClicked(object sender, EventArgs e) {
+		// Get the text from the Entry field
+		string inputPass = entPass1.Text;
+		string repeatPass = entPass2.Text;
+
+		// if IsPasswordValid is  false show alert with message "Password is not valid"
+		if (!IsPasswordValid(inputPass, repeatPass)) {
+			DisplayAlert("Validación de password", "Password is not valid", "OK");
+			return;
+		} else {
+			// show alert with message "Password is valid"
+			DisplayAlert("Validación de password", "Password is valid", "OK");
+		}
+		
+	}
+}
+
